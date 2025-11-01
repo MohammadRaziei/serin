5c4678d9e86f58a10b3443ba1b7a98136a754d52
Mohammad Raziei <mohammadraziei1375@gmail.com>
2025-11-01 12:15:42 +0000

    feat: add Python dictionary conversion and version info to serin bindings
    - Implement dict2value and value2dict functions for Python dictionary conversion
    - Add template functions for dictionary serialization/deserialization
    - Include version information macro for module version tracking
    - Update module name to use NB_MODULE_NAME macro
    - Fix namespace references to use serin:: prefix consistently

--- src/binding/main.cpp
+++ src/binding/main.cpp
@@ -9,18 +9,102 @@
 #include <nanobind/stl/variant.h>
 #include "serin.h"
 
+
+#define STRINGIFY(x) #x
+#define MACRO_STRINGIFY(x) STRINGIFY(x)
+
+
 namespace nb = nanobind;
-using namespace serin;
 
-NB_MODULE(serin, m) {
-    nb::class_<Value>(m, "Value")
+
+serin::Value dict2value(nb::handle obj) {
+    if (obj.is_none()) {
+        return Value(nullptr);
+    } else if (nb::isinstance<nb::bool_>(obj)) {
+        return Value(static_cast<bool>(nb::cast<bool>(obj)));
+    } else if (nb::isinstance<nb::int_>(obj)) {
+        return Value(static_cast<int64_t>(nb::cast<int64_t>(obj)));
+    } else if (nb::isinstance<nb::float_>(obj)) {
+        return Value(static_cast<double>(nb::cast<double>(obj)));
+    } else if (nb::isinstance<nb::str>(obj)) {
+        return Value(nb::cast<std::string>(obj));
+    } else if (nb::isinstance<nb::list>(obj)) {
+        serin::Array arr;
+        nb::list pylist = nb::cast<nb::list>(obj);
+        for (auto item : pylist)
+            arr.push_back(dict2value(item));
+        return Value(arr);
+    } else if (nb::isinstance<nb::dict>(obj)) {
+        serin::Object map;
+        nb::dict d = nb::cast<nb::dict>(obj);
+        for (auto [k, v] : d) {
+            map[nb::cast<std::string>(k)] = dict2value(v);
+        }
+        return Value(map);
+    }
+    throw std::runtime_error("Unsupported Python type for serin::Value conversion");
+}
+
+nb::object value2dict(const serin::Value& val) {
+    if (val.isPrimitive()) {
+        const auto& p = val.asPrimitive();
+        return std::visit([](auto&& arg) -> nb::object {
+            using T = std::decay_t<decltype(arg)>;
+            if constexpr (std::is_same_v<T, std::nullptr_t>) return nb::none();
+            else return nb::cast(arg);
+        }, p);
+    } else if (val.isArray()) {
+        nb::list out;
+        for (const auto& e : val.asArray())
+            out.append(value2dict(e));
+        return std::move(out);
+    } else if (val.isObject()) {
+        nb::dict out;
+        for (const auto& kv : val.asObject())
+            out[nb::cast(kv.first)] = value2dict(kv.second);
+        return std::move(out);
+    }
+    throw std::runtime_error("Unknown serin::Value type");
+}
+
+
+template<typename Func, typename... Args>
+std::string dumpsDict(Func f, const serin::Object& dict, Args&&... args) {
+    serin::Value v(dict);
+    return f(v, std::forward<Args>(args)...);
+}
+
+template<typename Func, typename... Args>
+std::string dumpDict(Func f, const serin::Object& dict, Args&&... args) {
+    serin::Value v(dict);
+    return f(v, std::forward<Args>(args)...);
+
+// loadsXDict : str -> dict
+template<typename Func, typename... Args>
+serin::Object loadsDict(Func f, const std::string& s, Args&&... args) {
+    serin::Value v = f(s, std::forward<Args>(args)...);
+    return v.asObject();
+}
+
+
+
+NB_MODULE(NB_MODULE_NAME, m) {
+    nb::class_<serin::Value>(m, "Value")
     .def(nb::init<>())
-    .def("is_object", &Value::isObject)
-    .def("is_array", &Value::isArray)
-    .def("is_primitive", &Value::isPrimitive);
+    .def("is_object", &serin::Value::isObject)
+    .def("is_array", &serin::Value::isArray)
+    .def("is_primitive", &serin::Value::isPrimitive);
 
-    m.def("value_loads_json", &loadsJson);
-    m.def("value_dumps_json", &dumpsJson);
-    m.def("value_loads_toon", &loadsToon);
-    m.def("value_dumps_toon", &dumpsToon);
+    m.def("value_loads_json", &serin::loadsJson);
+    m.def("value_dumps_json", &serin::dumpsJson);
+    m.def("value_loads_toon", &serin::loadsToon);
+    m.def("value_dumps_toon", &serin::dumpsToon);
+
+    m.def("loads_json", &serin::loadsJson)
+
+    #ifdef VERSION_INFO
+        m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
+    #else
+        m.attr("__version__") = "dev";
+    #endif
 }
